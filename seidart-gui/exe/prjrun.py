9d2b20426abab2ddd5a21ab03b2aa94dde1573b3
Jens Hansen <jens2798@gmail.com>
2020-02-26 16:36:52 -0500

    still don't work
    see above

--- seidart-gui/exe/prjrun.py
+++ seidart-gui/exe/prjrun.py
@@ -13,7 +13,7 @@ from subprocess import call
 #import materials.material_functions as mf
 
 # Modeling modules
-import seismicFDTD2D as seis2d
+from fdtd import seismicFDTD2D as seis2d
 from fdtd import emFDTD2D as em2d
 
 # -------------------------- Command Line Arguments ---------------------------
